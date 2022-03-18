46b341de4b7469422e448143e1b7fd928326e170
UtkucanSahinoglu <utkucannsahinoglu@gmail.com>
2022-03-18 15:22:34 +0300

    boş

--- DatabaseBuilder/AccountDatabaseBuilder.cs
+++ DatabaseBuilder/AccountDatabaseBuilder.cs
@@ -14,9 +14,6 @@ public static class AccountDatabaseBuilder
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.Email).IsRequired();
                 entity.Property(e => e.Password).IsRequired();
-              
-
-
 
             });
         modelBuilder.Entity<Account>().HasData(
