84fbb0695760fabf94022b3f6735c8bf489c55be
UtkucanSahinoglu <utkucannsahinoglu@gmail.com>
2022-03-18 12:51:19 +0300

    adres

---
+++ DatabaseBuilder/AddressDatabaseBuilder.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+public static class AddressDatabaseBuilder
+{
+    public static void TableBuilder(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Address>(entity =>
+        {
+            entity.HasKey(e =>e.Id);
+            entity.Property(e =>e.Name);
+            entity.Property(e =>e.OpenAddress);
+            entity.HasOne(e =>e.City).WithMany(e =>e.Addresses).HasForeignKey(e =>e.CityId);
+            entity.HasOne(e =>e.District).WithMany(e =>e.Addresses).HasForeignKey(e =>e.DistrictId);
+        });
+        modelBuilder.Entity<City>(entity =>
+        {
+            entity.HasKey(e =>e.Id);
+            entity.Property(e =>e.Name);
+
+        });
+        modelBuilder.Entity<District>(entity =>
+        {
+            entity.HasKey(e =>e.Id);
+            entity.Property(e =>e.Name);
+            entity.HasOne(e =>e.City).WithMany(e =>e.District).HasForeignKey(e =>e.CityId);
+        });
+    }
+}
